f3ac5cffd005aad468fa883e34904cf148cc4274
Tanya <wendetta_01@mail.ru>
2023-02-07 16:40:01 +0300

    Task 43

---
+++ Task43/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения
+// двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+
+int b1 = new Random().Next(0, 9);
+int b2 = new Random().Next(0, 9);
+int k1 = new Random().Next(0, 9);
+int k2 = new Random().Next(0, 9);
+
+void IntersectionPoint(int b1, int b2, int k1, int k2)
+{
+int y = 0;
+int x = 0;
+x = (b2 - b1) / (k2 - k1);
+y = k1 * x + b1;
+Console.WriteLine($"({x},{y})");
+}
+
+IntersectionPoint(b1, b2, k1, k2);
